c893129753bebf4d00150bbda6d046fc2e713b7d
dmorlo <github@dmorlo.com>
2015-10-18 15:36:41 +1100

    Update module path

--- pkg/cryptstate/mode_ocb2.go
+++ pkg/cryptstate/mode_ocb2.go
@@ -7,7 +7,7 @@ package cryptstate
 import (
 	"crypto/aes"
 	"crypto/cipher"
-	"mumble.info/grumble/pkg/cryptstate/ocb2"
+	"github.com/dmorlo/grumble/pkg/cryptstate/ocb2"
 )
 
 // ocb2Mode implements the OCB2-AES128 CryptoMode
